24214d170fdef05d9b0dd2de582c6b5548d34f87
Beesexcellentcodeingadventure <118213243+Beesexcellentcodeingadventure@users.noreply.github.com>
2025-09-27 19:37:08 -0400

    Add files via upload

---
+++ mad_lib.py
@@ -0,0 +1,20 @@
+#Monster Mad Lib
+#Change 5,9,10,11,12,13
+adjective = (" Give me a adjective.")
+verb = ("Give me a verb.")
+adjective_2 = ("Give me another adjective.")
+noun = ("Give me a noun.")
+color = ("What's your favoite color?")
+plural_noun = (" Give me a plural noun.")
+verb_2 = ("Give me another verb.")
+adjective_3 = ("Give me another adjective.")
+verb_3 = ("Give me another verb.")
+noun_2 = ("Give me another noun.")
+adjective_4 = ("Give me another adjective.")
+
+
+
+#print("Tonight is the night when all of the"+ adjective)
+#print("MONSTER come out to" + verb)
+#^^^This did not have the out come I wanted. What did I do wrong?
+# Out come I want: Tonight is the night when all the "adjective" MONSTER come out to "verb", "adjective" WICHES...  
